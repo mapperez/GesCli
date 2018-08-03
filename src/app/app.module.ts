b9bfa857648fdf54dd8301e7f5676a050b9ab432
Mario Antonio Perez Perez <mapperez@gmail.com>
2018-08-03 11:49:01 -0400

    Rev. 001

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './shared/layout/header/header.component';
+import { SidebarComponent } from './shared/layout/sidebar/sidebar.component';
+import { BreadcrumsComponent } from './shared/layout/breadcrums/breadcrums.component';
+import { NotfoundComponent } from './shared/notfound/notfound.component';
+import { HomeComponent } from './pages/home/home.component';
+import { APP_ROUTES } from './app.routes';
+import { PagesComponent } from './pages/pages.component';
+import { DashboradComponent } from './pages/dashborad/dashborad.component';
+import { GraficasComponent } from './pages/graficas/graficas.component';
+import { RegisterComponent } from './register/register.component';
+
+
+
+//Rutas
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NotfoundComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumsComponent,
+    PagesComponent,
+    HomeComponent,
+    DashboradComponent,
+    GraficasComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/layout/breadcrums/breadcrums.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcrums',
+  templateUrl: './breadcrums.component.html'
+})
+export class BreadcrumsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
