0a34056ee179814fc73c1088245c17599d5e3a88
Mario Antonio Perez Perez <mapperez@gmail.com>
2018-08-03 12:51:51 -0400

    Modificacion Modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+//Componentes Raiz
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { HeaderComponent } from './shared/layout/header/header.component';
-import { SidebarComponent } from './shared/layout/sidebar/sidebar.component';
-import { BreadcrumsComponent } from './shared/layout/breadcrums/breadcrums.component';
-import { NotfoundComponent } from './shared/notfound/notfound.component';
-import { HomeComponent } from './pages/home/home.component';
-import { APP_ROUTES } from './app.routes';
-import { PagesComponent } from './pages/pages.component';
-import { DashboradComponent } from './pages/dashborad/dashborad.component';
-import { GraficasComponent } from './pages/graficas/graficas.component';
 import { RegisterComponent } from './register/register.component';
 
 
+//Modulos
+import {PagesModule} from './pages/pages.module'
+
 
 //Rutas
+import { APP_ROUTES } from './app.routes';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NotfoundComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumsComponent,
-    PagesComponent,
-    HomeComponent,
-    DashboradComponent,
-    GraficasComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,32 +1,14 @@
 import {RouterModule, Routes} from '@angular/router'
 
 //Componentes
-import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './login/login.component';
 import { NotfoundComponent } from './shared/notfound/notfound.component';
-import { PagesComponent } from './pages/pages.component';
-import { DashboradComponent } from './pages/dashborad/dashborad.component';
-import { GraficasComponent } from './pages/graficas/graficas.component';
 import { RegisterComponent } from './register/register.component';
 
 const appRoutes: Routes= [
-
-  {
-    path: '',
-    component: PagesComponent,
-    children: [
-      {path: 'home', component: HomeComponent  },
-      {path: 'dashboard', component: DashboradComponent  },
-      {path: 'graficas', component: GraficasComponent  },
-      {path: '', redirectTo: 'home', pathMatch: 'full'  },
-    ]
-  },
-
   {path: 'login', component: LoginComponent  },
   {path: 'register', component: RegisterComponent  },
   {path: '**', component: NotfoundComponent  },
-
-
 ];
 
 export const APP_ROUTES = RouterModule.forRoot(appRoutes, {useHash: true})
